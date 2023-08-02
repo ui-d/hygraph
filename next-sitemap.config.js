b739669a7f81cd9fa2d4109cf81130f2867b2ce9
ui-d <dawiddeveloper@gmail.com>
2023-08-02 19:44:39 +0200

    project scaffold

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('next-sitemap').IConfig}
+ */
+module.exports = {
+  siteUrl: '',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <main>
+        <h1 className="text-center">Home Page</h1>
+    </main>
+  );
+}
