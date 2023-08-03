abc5335549aaa3f0264d6cf12a3c39a88e7edb47
ui-d <dawiddeveloper@gmail.com>
2023-08-03 19:19:49 +0200

    chore: query changed

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -3,7 +3,6 @@ import { gql } from 'graphql-request';
 const pageQuery = gql`
   query Page($slug: String!) {
     page(where: { slug: $slug }) {
-      title
       sections {
         ... on Section {
           id
