668b7198165036a48f44dd8ce7f7d4c16f089ba9
ui-d <dawiddeveloper@gmail.com>
2023-08-03 12:24:36 +0200

    chore: tailwind configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
+import { Inter, Space_Grotesk } from '@next/font/google';
 import { Metadata } from 'next';
 import * as React from 'react';
 
 import '@/styles/globals.css';
-import '@/styles/colors.css';
 
 import { siteConfig } from '@/constant/config';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
+});
 
 export const metadata: Metadata = {
   title: {
@@ -44,7 +54,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>{children}</body>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main>
-      <h1 className='text-center'>Home Page</h1>
+      <h1 className='font-inter text-center'>Home Page</h1>
     </main>
   );
 }
