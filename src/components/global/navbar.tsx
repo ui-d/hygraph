cc1cb9c763e576fa27195373739f90a1064bb9a2
ui-d <dawiddeveloper@gmail.com>
2023-08-05 19:07:18 +0200

    style: nav spacing reworked

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
     <nav className={cn('border-light-gray fixed flex w-full border bg-white')}>
       <Container>
         <div className={cn('flex h-20 w-full items-center justify-between')}>
-          <div className={cn('flex items-center gap-2')}>
+          <div className={cn('flex items-center gap-3')}>
             <Link href='/'>
               <Image alt='logo' src='/svg/Logo.svg' width={100} height={100} />
             </Link>
@@ -30,7 +30,7 @@ export const Navbar = () => {
               </a>
             ))}
           </div>
-          <div className={cn('flex items-center gap-7')}>
+          <div className={cn('flex items-center gap-4')}>
             <a
               className={cn(
                 'font-inter text-medium-blue hover:bg-gray hidden rounded-md px-3 py-2 text-base md:block'
