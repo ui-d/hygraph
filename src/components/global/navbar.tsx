8983902a2dc3962f97889a3156cfe5b2c3da2f8a
ui-d <dawiddeveloper@gmail.com>
2023-08-05 18:42:57 +0200

    style: mobile navbar updated

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,29 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 
 import { Button } from '@/components/button';
+import { Container } from '@/components/global/container';
 
 import Items from './navbar-navigation.json';
 
 export const Navbar = () => {
   const { items } = Items;
   return (
-    <>
-      <nav className={cn('fixed flex h-20 w-full border bg-white')}>
-        <div
-          className={cn(
-            'mx-auto flex w-full max-w-7xl items-center justify-between'
-          )}
-        >
-          <div className={cn('flex items-center gap-3')}>
-            <Image alt='logo' src='/svg/Logo.svg' width={100} height={100} />
+    <nav className={cn('border-light-gray fixed flex w-full border bg-white')}>
+      <Container>
+        <div className={cn('flex h-20 w-full items-center justify-between')}>
+          <div className={cn('flex items-center gap-2')}>
+            <Link href='/'>
+              <Image alt='logo' src='/svg/Logo.svg' width={100} height={100} />
+            </Link>
             {items.map((item) => (
               <a
                 href={item.url}
                 key={item.id}
                 className={cn(
-                  'text-mediumBlue font-inter rounded-md px-3 py-2 text-base hover:bg-gray-100 hover:text-gray-800'
+                  'text-medium-blue font-inter hover:bg-gray hidden rounded-md px-3 py-2 text-base hover:text-gray-800 md:block'
                 )}
               >
                 {item.name}
@@ -31,13 +31,18 @@ export const Navbar = () => {
             ))}
           </div>
           <div className={cn('flex items-center gap-7')}>
-            <a className={cn('font-inter text-mediumBlue text-base')} href='#'>
+            <a
+              className={cn(
+                'font-inter text-medium-blue hover:bg-gray hidden rounded-md px-3 py-2 text-base md:block'
+              )}
+              href='#'
+            >
               Log in
             </a>
             <Button url='#' size='sm' text='Sign up' />
           </div>
         </div>
-      </nav>
-    </>
+      </Container>
+    </nav>
   );
 };
