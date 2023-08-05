ee1be50f5d2d41a3e7c3118e4bbdb88b4a22af44
ui-d <dawiddeveloper@gmail.com>
2023-08-05 19:06:08 +0200

    feat: feature section ready

--- src/components/featureSection.tsx
+++ src/components/featureSection.tsx
@@ -4,57 +4,113 @@ import { cn } from '@/lib/utils';
 
 import { Button } from '@/components/button';
 
-type FeatureActionButton = {
+type ActionButtonProps = {
   id: string;
   size: 'sm' | 'md' | 'lg';
   text: string;
+};
+
+type YoutubeType = {
+  link: string;
+};
+
+type ImageType = {
+  image: { url: string };
 };
 
 type FeatureSectionProps = {
   label?: string;
   title: string;
   description: string;
-  order: boolean;
-  featureActionButton?: FeatureActionButton;
+  reversed: boolean;
+  media: YoutubeType | ImageType; // changed here
+  featureActionButton?: ActionButtonProps;
+};
+
+const YouTubeEmbed = ({ link }: YoutubeType) => {
+  let videoId = '';
+
+  if (link.includes('youtu.be')) {
+    videoId = link.split('youtu.be/')[1];
+  } else if (link.includes('youtube.com')) {
+    videoId = new URL(link).searchParams.get('v') || '';
+  }
+
+  return (
+    <div>
+      <iframe
+        width='480'
+        height='320'
+        src={`https://www.youtube.com/embed/${videoId}`}
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        title='Embedded youtube'
+      />
+    </div>
+  );
 };
 
 export const FeatureSection = ({
-  title,
   label,
+  title,
   description,
+  reversed,
+  media,
   featureActionButton,
-  order,
 }: FeatureSectionProps) => {
   return (
-    <section className={cn('flex justify-between gap-20')}>
-      <div className={cn('flex-1', { 'order-2': order })}>
-        {!!label && (
+    <section
+      className={cn(
+        'flex flex-col items-center justify-between gap-24 md:flex-row'
+      )}
+    >
+      <div className={cn('flex-1', { 'order-2': reversed })}>
+        {label && (
           <p className={cn('font-space-grotesk text-purple text-lg font-bold')}>
             {label}
           </p>
         )}
         <h2
           className={cn(
-            'font-inter text-darkerBlue pb-5 text-2xl font-semibold'
+            'font-inter text-darker-blue pb-4 text-2xl font-semibold'
           )}
         >
           {title}
         </h2>
-        <p className={cn('text-mediumBlue font-inter pb-10 text-base')}>
+        <p className={cn('text-medium-blue font-inter text-lg-narrow pb-7')}>
           {description}
         </p>
-        {!!featureActionButton && (
+        {featureActionButton && (
           <Button
             url='#'
             text={featureActionButton.text}
             id={featureActionButton.id}
             size={featureActionButton.size}
+            className={cn('w-full sm:w-auto')}
           />
         )}
       </div>
-      <figure className={cn('flex-1', { 'order-1': order })}>
-        <Image src='' width='100' height='100' alt='placeholder' />
-      </figure>
+      {media && (
+        <figure
+          className={cn('bg-gray flex flex-1 justify-center rounded-xl py-10', {
+            'order-1': reversed,
+          })}
+        >
+          {'link' in media ? (
+            <YouTubeEmbed link={media.link} />
+          ) : (
+            media.image && (
+              <Image
+                src={media.image.url}
+                alt='Feature Image'
+                width={480}
+                height={320}
+              />
+            )
+          )}
+        </figure>
+      )}
     </section>
   );
 };
