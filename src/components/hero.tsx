670aae8c96bb16e60e92284387e899da1d6ff61c
ui-d <dawiddeveloper@gmail.com>
2023-08-05 19:06:58 +0200

    feat: hero section ready

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,10 @@
+import { cn } from '@/lib/utils';
+
+import { Button } from '@/components/button';
+
 type ActionButton = {
   id: string;
-  size: string;
+  size: 'sm' | 'md' | 'lg';
   text: string;
 };
 
@@ -12,14 +16,32 @@ type HeroProps = {
 
 export const Hero = ({ title, description, actionButtons }: HeroProps) => {
   return (
-    <>
-      <p>{title}</p>
-      <p>{description}</p>
-      {actionButtons?.map((actionButton) => (
-        <a key={actionButton.id} href='#'>
-          {actionButton.text}
-        </a>
-      ))}
-    </>
+    <section className={cn('pt-20 text-center')}>
+      <h1
+        className={cn(
+          'font-inter text-darker-blue pb-6 text-3xl font-semibold tracking-tight md:text-4xl'
+        )}
+      >
+        {title}
+      </h1>
+      <p
+        className={cn(
+          'font-inter text-light-blue mx-auto max-w-3xl pb-12 text-xl'
+        )}
+      >
+        {description}
+      </p>
+      <div className={cn('flex items-center justify-center gap-5')}>
+        {actionButtons?.map((actionButton) => (
+          <Button
+            key={actionButton.id}
+            size={actionButton.size}
+            text={actionButton.text}
+            className={cn('w-full md:w-auto')}
+            url='#'
+          />
+        ))}
+      </div>
+    </section>
   );
 };
