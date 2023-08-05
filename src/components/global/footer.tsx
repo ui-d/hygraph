7195596f06571d771f3325eedec50960381893ad
ui-d <dawiddeveloper@gmail.com>
2023-08-05 18:48:18 +0200

    style: footer updated on mobile

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -1,36 +1,40 @@
+import Link from 'next/link';
+
 import { cn } from '@/lib/utils';
 
-import Items from './footer-navigation.json';
+import { Container } from '@/components/global/container';
+
+import navigationItems from './footer-navigation.json';
 
 export const Footer = () => {
-  const { items } = Items;
+  const { items } = navigationItems;
   return (
-    <>
-      <nav className={cn('fixed flex h-20 w-full bg-white')}>
+    <Container>
+      <section className={cn('flex w-full')}>
         <div
           className={cn(
-            'mx-auto flex w-full max-w-7xl items-center justify-between border-t'
+            'border-light-gray mx-auto flex w-full flex-col-reverse items-center justify-between gap-8 border-t py-8 md:flex-row'
           )}
         >
-          <p className={cn('font-inter text-lightBlue text-base')}>
+          <p className={cn('font-inter text-light-blue text-base')}>
             © 2023 Acme Co. All rights reserved.
           </p>
 
           <div className={cn('flex items-center gap-4')}>
             {items.map((item) => (
-              <a
+              <Link
                 key={item.id}
                 href={item.url}
                 className={cn(
-                  'font-inter text-lightBlue text-base hover:text-gray-900'
+                  'font-inter text-light-blue hover:text-medium-blue text-base'
                 )}
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
         </div>
-      </nav>
-    </>
+      </section>
+    </Container>
   );
 };
