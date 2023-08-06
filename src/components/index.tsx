b64042f6ea078f2c9bf40f8f1da49323b4e6e53a
ui-d <dawiddeveloper@gmail.com>
2023-08-06 13:55:54 +0200

    refactor: path changed

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 import { Callout } from '@/components/callout';
-import { FeatureSection } from '@/components/featureSection';
+import { FeatureSection } from '@/components/feature-section';
 import { Hero } from '@/components/hero';
 
 export { Callout, FeatureSection, Hero };
