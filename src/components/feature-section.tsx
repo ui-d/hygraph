82e8f43a66c3ad194ae781f03664af69819a073b
ui-d <dawiddeveloper@gmail.com>
2023-08-06 13:56:40 +0200

    refactor: deprecated attr removed

--- src/components/feature-section.tsx
+++ src/components/feature-section.tsx
@@ -49,7 +49,6 @@ const YouTubeEmbed = ({ link }: YoutubeType) => {
           border: 0,
         }}
         src={`https://www.youtube.com/embed/${videoId}`}
-        frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         allowFullScreen
         title='Embedded youtube'
